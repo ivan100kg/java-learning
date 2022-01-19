a7ba6309d4379d5e1d04abafbdb87f9ced377bc4
ivar <ivar110kg@gmail.com>
2022-01-19 21:47:02 +0400

    Add homework for Lesson2

---
+++ udemy/Homeworks/Lesson2.java
@@ -0,0 +1,12 @@
+/*
+ */
+package Homeworks;
+
+public class Lesson2 {
+    public static void main(String[] args){
+        byte b1 = 0b1100;   // 2
+        byte b3 = 014;      // 8
+        byte b2 = 12;       // 10
+        byte b4 = 0xC;      // 16
+    }
+}
