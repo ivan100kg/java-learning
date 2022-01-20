ea047668d73e13ee5024c902e1bcfc454c9b180d
ivar <ivar110kg@gmail.com>
2022-01-20 19:30:55 +0400

    Add Test6.java

---
+++ udemy/Lesson3/Test6.java
@@ -0,0 +1,20 @@
+package Lesson3;
+
+public class Test6 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        
+        boolean bool1 = a>b;
+        boolean bool2 = a<b;
+        boolean bool3 = a<=b;   // >=
+        boolean bool4 = a==b;
+        boolean bool5 = a!=b;
+        
+        
+        System.out.println(bool1 && bool2); // and
+        System.out.println(bool1 || bool2); // or
+        System.out.println(!bool1);         // not
+        
+    }
+}
