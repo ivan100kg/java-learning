c048b63351a0b82308450a65e874faa7dc5a8c25
ivar <ivar110kg@gmail.com>
2022-01-20 19:30:44 +0400

    Add Test4.java

---
+++ udemy/Lesson3/Test4.java
@@ -0,0 +1,21 @@
+
+package Lesson3;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int c = a+b;
+        int d = a-b;
+        int e = a*b;
+        int f = a/b;    // целая часть
+        int ost = a%b;  // остаток
+        float g = (float)a/(float)b;
+        ost++;
+        --ost;
+        
+        
+        
+        System.out.println(ost);
+    }
+}
