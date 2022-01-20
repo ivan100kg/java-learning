9f76cb084559a1ea02cf1bda6a80d9f461597d6f
ivar <ivar110kg@gmail.com>
2022-01-20 19:31:06 +0400

    Add Test8.java

---
+++ udemy/Lesson3/Test8.java
@@ -0,0 +1,14 @@
+package Lesson3;
+
+public class Test8 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 3;
+        int c = (++a - b)*2;    // приоритет операций
+        
+        char s = 'a';
+        
+        System.out.println(s+a);    // 'a' переводится к числу ASCII
+        
+    }
+}
