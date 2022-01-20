f08e6c0f2705d534e3c6c469cca15b05ad6d1b30
ivar <ivar110kg@gmail.com>
2022-01-20 19:30:50 +0400

    Add Test5.java

---
+++ udemy/Lesson3/Test5.java
@@ -0,0 +1,20 @@
+
+package Lesson3;
+
+public class Test5 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        b += 2;
+        a -= 2;
+        a *= 7;
+        b /= 2;
+        a %= 8;
+        a = b = 67;
+        
+        long l = 100L;
+         
+        
+        System.out.println(a);
+    }
+}
