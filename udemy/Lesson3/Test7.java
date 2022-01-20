9a23c400a23e1821832bb7659f59148d089dc73b
ivar <ivar110kg@gmail.com>
2022-01-20 19:31:00 +0400

    Add Test7.java

---
+++ udemy/Lesson3/Test7.java
@@ -0,0 +1,15 @@
+package Lesson3;
+
+public class Test7 {
+    public static void main(String[] args) {
+        boolean b1 = true;
+        boolean b2 = true;
+        boolean b3 = false;
+        boolean b4 = false;
+        
+        System.out.println(b1 ^ b3); // исключающее ИЛИ
+        
+        System.out.println(1 & 2); // побитовое И 01&10=0
+        System.out.println(4 | 8);  // побитовое ИЛИ
+    }
+}
