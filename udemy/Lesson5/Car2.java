a6f27ff94faed99f733f09fb1d8bfc9ec5481ed3
ivar <ivar110kg@gmail.com>
2022-01-22 05:50:47 +0400

    Add Car2.java

---
+++ udemy/Lesson5/Car2.java
@@ -0,0 +1,35 @@
+
+package Lesson5;
+
+public class Car2 {
+    String color;
+    String engine;
+    int speed;
+    
+    int gas(int x){
+        return speed+=x;
+    }
+    
+    int tor(int x){
+        return speed-=x;
+    }
+    
+    void show(){
+        System.out.println("color: "+color+" engine: "+engine+" speed: "+speed);
+    }
+}
+class TestCar2{
+    public static void main(String[] args) {
+        Car2 c1 = new Car2();
+        c1.color = "white";
+        c1.engine = "1.6";
+        c1.speed = 60;
+        
+        c1.show();
+        c1.gas(20);
+        c1.show();
+        c1.tor(40);
+        c1.show();
+    }
+    
+}
