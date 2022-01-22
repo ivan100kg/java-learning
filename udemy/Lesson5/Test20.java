968e82bf3db67f41603f5df9d0b1ab50294a0863
ivar <ivar110kg@gmail.com>
2022-01-22 05:50:37 +0400

    Add Test20.java

---
+++ udemy/Lesson5/Test20.java
@@ -0,0 +1,23 @@
+
+package Lesson5;
+
+public class Test20 {
+    int sum(int a, int b, int c){
+        int result = a+b+c;
+        return result;
+    }
+    
+    int average(int a, int b, int c){
+        return sum(a,b,c)/3;
+    }
+}
+
+class Test21{
+    public static void main(String[] args) {
+        Test20 t = new Test20();
+        int s = t.sum(1, 2, 3);
+        System.out.println(s);
+        System.out.println(t.sum(4, 5, 6));
+        System.out.println(t.average(4, 5, 6));
+    }
+}
