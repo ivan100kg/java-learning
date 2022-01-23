165c9f4aa9a85f80588f7480f0561526e3115664
ivar <ivar110kg@gmail.com>
2022-01-23 12:24:11 +0400

    Update Car.java

--- udemy/Lesson4/Car.java
+++ udemy/Lesson4/Car.java
@@ -1,15 +1,23 @@
 package Lesson4;
 
 public class Car {
-    String color = "red";
-    String engine = "V6";
-    float wheight = 1000;
+    Car(){System.out.println("Without params constructor");}    // default constructor
+    Car(String color, String eng, float wh){
+        System.out.println("Custom constructor");
+        this.color = color; // this.color
+        this.engine = eng;  //  |
+        wheight = wh;       //  |
+    }                       //  |
+    String color;   // <--------+
+    String engine;
+    float wheight;
+    
 }
 
 class CarTest{
     public static void main(String[] args) {
         Car car1 = new Car();
-        Car car2 = new Car();
+        Car car2 = new Car("pink","1.2",900);
         Car car3 = car1;
         Car car4;   // undefined
         
@@ -20,5 +28,8 @@ class CarTest{
         System.out.println("wheight " + car3.wheight);
         System.out.println(car3.color);
         System.out.println(new Car().color);    // without ref
+        
+        System.out.println();
+        System.out.println(car2.color);
     }
 }
