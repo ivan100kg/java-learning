314d0b191f5a3a2aaf1badb7253689e3de7aa835
ivar <ivar110kg@gmail.com>
2022-01-24 10:56:41 +0400

    Add Employee.java

---
+++ udemy/Lesson6/Employee.java
@@ -0,0 +1,62 @@
+package Lesson6;
+
+public class Employee {
+    // constructor overloading
+    Employee(int id, String surname, int age, double salary, 
+            String departament){
+        System.out.println("0 constructor");        
+        this.id = id;
+        this.surname = surname;
+        this.age = age;
+        this.salary = salary;
+        this.departament = departament;
+    }
+    
+    Employee(int id, String surname, int age){
+        this(id, surname, age, 0.0, null);
+        System.out.println("1st constructor");        
+//        this.id = id;
+    }
+    
+    Employee(String surname, int age){
+        this(0, surname, age, 0.0, null);
+        System.out.println("2nd constructor");
+//        this.age = age;
+    }
+    
+    Employee(String surname){
+        this(0, surname, 0, 0.0, null);
+        System.out.println("3rd constructor");
+//        this.surname = surname;
+    }
+    
+    int id;
+    String surname;
+    int age;
+    double salary;
+    String departament;
+    
+    void showInfo(){
+        System.out.println("id: "+ id);
+        System.out.println("surname : "+ surname);
+        System.out.println("age : "+ age);
+        System.out.println("salary : "+ salary);
+        System.out.println("departament : "+ departament);
+                
+    }
+}
+
+
+class EmployeeTest{
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Bond", 67);
+        emp1.showInfo();
+        
+        Employee emp2 = new Employee("Bob");
+        emp2.showInfo();
+        
+        Employee emp3 = new Employee(1, "Henry", 34);
+        emp3.showInfo();
+        
+    }
+}
