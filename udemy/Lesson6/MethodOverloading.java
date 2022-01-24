00c6ba382a2389980f254376f03f73ef79ceca90
ivar <ivar110kg@gmail.com>
2022-01-24 10:57:58 +0400

    Update MethodOverloading.java

--- udemy/Lesson6/MethodOverloading.java
+++ udemy/Lesson6/MethodOverloading.java
@@ -4,12 +4,28 @@ public class MethodOverloading {
     void show(int i1){
         System.out.println(i1);
     }
+    
+    
+    void show(boolean b1){
+        System.out.println(b1);
+    }
+    
+    void show(String s1){
+        System.out.println(s1);
+    }
 }
 
 class MethodOverloadingTest{
     public static void main(String[] args) {
         MethodOverloading mO = new MethodOverloading();
-        mO.show(100);
+        
+        int a = 500;
+        boolean b = true;
+        String s = "lalala";
+        
+        mO.show(a);
+        mO.show(b);
+        mO.show(s);
     }
 }
 
