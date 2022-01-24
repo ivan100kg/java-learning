d2b015cfe3ba826ee606d068b9dcfc33abd3464e
ivar <ivar110kg@gmail.com>
2022-01-24 10:57:41 +0400

    Add MethodOverloading.java

---
+++ udemy/Lesson6/MethodOverloading2.java
@@ -0,0 +1,20 @@
+package Lesson6;
+
+public class MethodOverloading2 {
+    protected int sum(int a, int b){
+        return a+b;
+    }
+    
+    public String sum(String a, String b){
+        return a+b;
+    }
+}
+
+class MethodOverloading2Test{
+    public static void main(String[] args) {
+        MethodOverloading2 mO2 = new MethodOverloading2();
+        
+        System.out.println(mO2.sum(5, 7));
+        System.out.println(mO2.sum("Hi", " There!"));
+    }
+}
