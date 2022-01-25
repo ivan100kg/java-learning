d7a338519e76e6f676e3651ae07751ea6fc9343f
HERO <ivar110kg@gmail.com>
2022-01-25 13:55:32 +0400

    Add Car.java

---
+++ udemy/Lesson8/Car.java
@@ -0,0 +1,20 @@
+package Lesson8;
+
+public class Car {
+
+    String color = "blue";
+    String engine = "V8";
+}
+
+class Human {
+
+    String name = "Ivan";
+    final Car c = new Car();
+
+    public static void main(String[] args) {
+        Human h1 = new Human();
+        // h1.c = new Car();    // forbidden
+        // h1.c = new Car();
+        h1.c.engine = "V12";
+    }
+}
