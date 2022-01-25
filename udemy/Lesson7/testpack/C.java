21cef9c597c9b958ae263f774ef847d97d6944d1
ivar <ivar110kg@gmail.com>
2022-01-25 07:30:38 +0400

    add C.java

---
+++ udemy/Lesson7/testpack/C.java
@@ -0,0 +1,11 @@
+package Lesson7.testpack;
+
+//import Lesson7.Employee;
+
+public class C {
+    public static void main(String[] args) {
+        Lesson7.Employee emp = new Lesson7.Employee(200);
+        System.out.println(emp.salary);
+        emp.doubleSalary();
+    }
+}
