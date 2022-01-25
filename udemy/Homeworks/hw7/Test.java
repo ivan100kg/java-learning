63808b5c1c094f0489e34f7763a3bd1728cfe799
HERO <ivan110kg@gmail.com>
2022-01-25 11:42:44 +0400

    Complete hw7

---
+++ udemy/Homeworks/hw7/Test.java
@@ -0,0 +1,10 @@
+package Homeworks.hw7;
+
+public class Test {
+    public static void main(String[] args) {
+        Employee emp = new Employee(2,"Bob",0,2000,null);
+        emp.showSalary();
+        emp.showId();
+        emp.showSurname();
+    }
+}
