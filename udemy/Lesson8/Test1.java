6a6c47f11e4903f492db41e2182ab430a311d0a0
HERO <ivar110kg@gmail.com>
2022-01-25 13:54:15 +0400

    Add Test1.java

---
+++ udemy/Lesson8/Test1.java
@@ -0,0 +1,21 @@
+package Lesson8;
+
+public class Test1 {
+
+    public int a = 20;
+    public final int b = 40;    // сразу определяем
+
+    public void abc(short s) {
+        final byte b;   // локальные можно объявить так
+        b=10;
+        System.out.println(s + b);
+    }
+
+    public static void main(String[] args) {
+        Test1 t = new Test1();
+        t.a = t.a * 2;
+//        t.b = t.b * 2;
+        System.out.println(t.a);
+
+    }
+}
