013872d7bdcf25c7a4f6da775830e17dd9820981
ivar <ivar110kg@gmail.com>
2022-01-25 07:33:55 +0400

    Add AnotherClass.java

---
+++ udemy/Lesson7/AnotherClass.java
@@ -0,0 +1,9 @@
+package Lesson7;
+
+public class AnotherClass {
+    public static void main(String[] args) {
+        Employee emp = new Employee(200);
+        System.out.println(emp.salary);
+        emp.doubleSalary();
+    }
+}
