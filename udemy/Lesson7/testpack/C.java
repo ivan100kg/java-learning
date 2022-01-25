6a7d44ee393f42013ee3f282d291aa3b5e74d7d8
HERO <ivan110kg@gmail.com>
2022-01-25 10:20:37 +0400

    Replacing to new platform

--- udemy/Lesson7/testpack/C.java
+++ udemy/Lesson7/testpack/C.java
@@ -5,7 +5,7 @@ package Lesson7.testpack;
 public class C {
     public static void main(String[] args) {
         Lesson7.Employee emp = new Lesson7.Employee(200);
-        System.out.println(emp.salary);
+        // System.out.println(emp.salary);
         emp.doubleSalary();
     }
 }
