fe9edbca57e951875669644537068cf22fa694d5
HERO <ivar110kg@gmail.com>
2022-01-26 08:41:05 +0400

    Add Hw.java

---
+++ udemy/Homeworks/hw9/Hw.java
@@ -0,0 +1,19 @@
+package Homeworks.hw9;
+
+public class Hw {
+    public static void main(String[] args) {
+        Hw obj1 = new Hw();
+        Hw obj2 = new Hw();
+        Hw obj3 = new Hw();
+        Hw obj4 = new Hw();
+        Hw obj5 = new Hw();
+        Hw obj6 = new Hw();
+        Hw obj7 = new Hw();
+        Hw obj8 = new Hw();
+        Hw obj9 = new Hw();
+
+        
+    }    
+}
+
+
