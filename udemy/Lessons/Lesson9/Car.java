a9b856ecb35fc3f6f24e8930729c1f32bb68a483
HERO <ivar110kg@gmail.com>
2022-01-26 08:38:04 +0400

    Add Car.java

---
+++ udemy/Lessons/Lesson9/Car.java
@@ -0,0 +1,34 @@
+package Lesson9;
+
+public class Car {
+    private String color;
+    String engine;
+    static int count;
+
+    Car(String color, String engine) {
+        count++;
+        this.color = color;
+        this.engine = engine;
+    }
+
+    public void showColor() {
+        System.out.println("Color: " + color);
+    }
+
+    public void changeColor(String color) {
+        System.out.println("Color is changed to " + color);
+        int price = 5000;
+        this.color = color;
+        price += 1000;
+        int $s = 9;
+        int _$_ = 12;
+    }
+
+    public static void main(String[] args) {
+        Car car = new Car("blue", "V8");
+        car.changeColor("red");
+        car.showColor();
+        car.color = "blue";
+        car.showColor();
+    }
+}
