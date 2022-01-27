8d34f478f0f7b8c5e83a4c918bf743d6e8de1245
HERO <ivar110kg@gmail.com>
2022-01-27 08:42:01 +0400

    Add Student.java

---
+++ udemy/Lessons/Lesson11/Student.java
@@ -0,0 +1,39 @@
+package Lessons.Lesson11;
+
+public class Student {
+    String name;
+    int course;
+    double grade;
+
+    Student(String name, int course, double grade){
+        this.name = name;
+        this.course = course;
+        this.grade = grade;
+    }
+
+    public static void swap(Student s1, Student s2){
+        Student s3 = s1;
+        s1 = s2;
+        s2 = s3;
+    }
+
+    public static void changeName(Student st1){
+        st1.name = "Changed";
+    }
+
+    public static void main(String[] args) {
+        Student st1 = new Student("Igor", 1, 7.6);
+        Student st2 = new Student("Egor", 1, 5.3);
+       
+        System.out.println(st1.name);
+        swap(st1, st2);
+        System.out.println(st1.name);
+
+        System.out.println();
+
+        System.out.println(st2.name);
+        changeName(st2);
+        System.out.println(st2.name);
+
+    }
+}
