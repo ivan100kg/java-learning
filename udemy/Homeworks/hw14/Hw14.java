2128dbb277da4187e592a8b6cd3540251043f5ee
HERO <ivar110kg@gmail.com>
2022-01-28 12:57:29 +0400

    Do homework14

---
+++ udemy/Homeworks/hw14/Hw14.java
@@ -0,0 +1,25 @@
+package Homeworks.hw14;
+
+public class Hw14 {
+    static void time() {
+        HOUR:
+        for (int hour = 0; hour < 6; hour++) {
+            MIN:
+            for (int min = 0; min < 60; min++) {
+                if (hour > 1 && min != 0 && min % 10 == 0) {
+                    break HOUR;
+                }
+                for (int sec = 0; sec < 60; sec++) {
+                    if (sec * hour > min) {
+                        continue MIN;
+                    }
+                    System.out.println(hour + ":" + min + ":" + sec);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        time();
+    }
+}
