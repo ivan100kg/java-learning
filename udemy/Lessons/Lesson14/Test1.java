bf47132ae04e60dcf9d727c855dca63ec95efb1a
HERO <ivar110kg@gmail.com>
2022-01-28 11:09:48 +0400

    Add Test1.java

---
+++ udemy/Lessons/Lesson14/Test1.java
@@ -0,0 +1,20 @@
+package Lessons.Lesson14;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+        System.out.println("Lesson 14");
+
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i+") Lesson 14(for)");
+        }
+    }
+}
