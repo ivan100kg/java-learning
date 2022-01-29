6c9941c0b2037040a4b3565fbe51d473d804cd0e
HERO <ivar110kg@gmail.com>
2022-01-29 09:51:36 +0400

    Update Test8.java

---
+++ udemy/Lessons/Lesson15/Test8.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson15;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+public class Test8 {
+    public static void main(String[] args) {
+        // infinite
+        do
+            System.out.println("Hi");
+        while (true);
+
+    }
+}
