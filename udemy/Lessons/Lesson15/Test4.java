8237ab6c94281f56fc721ceadcef9d4b1b89fd79
HERO <ivar110kg@gmail.com>
2022-01-29 09:31:51 +0400

    Add Test4.java

---
+++ udemy/Lessons/Lesson15/Test4.java
@@ -0,0 +1,15 @@
+package Lessons.Lesson15;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int a = 5;
+        while (a++ < 10) {
+            System.out.println(a);
+        }
+
+        a = 5;
+        while (++a < 10) {
+            System.out.println(a);
+        }
+    }
+}
