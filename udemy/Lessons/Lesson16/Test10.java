f5a1a85bc705ae80f0289c03f4fa1696d585b2b8
HERO <ivar110kg@gmail.com>
2022-02-01 11:02:16 +0400

    Add Test10.java

---
+++ udemy/Lessons/Lesson16/Test10.java
@@ -0,0 +1,12 @@
+package Lessons.Lesson16;
+
+public class Test10 {
+    public static void main(String[] args) {
+        String x = "hi";
+        String y = "hi".trim();
+
+        System.out.println(x == y);
+        System.out.println(x.equals(y));
+
+    }
+}
