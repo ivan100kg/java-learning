37ab8123d5ce08b8d6c7627c97df9a1cc86c7c3b
HERO <ivar110kg@gmail.com>
2022-02-01 10:56:15 +0400

    Add Test9.java

---
+++ udemy/Lessons/Lesson16/Test9.java
@@ -0,0 +1,19 @@
+package Lessons.Lesson16;
+
+public class Test9 {
+    public static void main(String[] args) {
+        String s1 = new String("ok");
+        String s2 = new String("ok");
+
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+
+        String s3 = "ok";
+        String s4 = "ok";
+
+        System.out.println(s3 == s4);
+        System.out.println(s3.equals(s4));
+
+
+    }
+}
