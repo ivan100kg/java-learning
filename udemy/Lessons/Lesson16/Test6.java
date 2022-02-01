f374eb4d133efba3ef124733b5fc0e352fa88fda
HERO <ivar110kg@gmail.com>
2022-02-01 09:22:54 +0400

    Add Test6.java

---
+++ udemy/Lessons/Lesson16/Test6.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson16;
+
+public class Test6 {
+    public static void main(String[] args) {
+        String s = "Hello";
+        String s2 = s.toLowerCase();
+        String s3 = s.toUpperCase();
+
+        boolean b = s.contains("He");
+
+        System.out.println(b);
+    }
+}
