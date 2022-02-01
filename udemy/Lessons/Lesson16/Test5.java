ecc3b90b40aad94cec417466f6207ce2e9068bc3
HERO <ivar110kg@gmail.com>
2022-02-01 09:13:25 +0400

    Add Test5.java

---
+++ udemy/Lessons/Lesson16/Test5.java
@@ -0,0 +1,10 @@
+package Lessons.Lesson16;
+
+public class Test5 {
+    public static void main(String[] args) {
+        String s1 = null;
+        System.out.println(s1 += "ok");
+
+        System.out.println("" + null + false);  // nullfalse
+    }
+}
