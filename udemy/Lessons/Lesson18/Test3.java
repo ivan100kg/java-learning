40ddaec5c4e6384f1cb3c774fe784da9890add55
HERO <ivar110kg@gmail.com>
2022-02-03 20:49:09 +0400

    Add Test3.java

---
+++ udemy/Lessons/Lesson18/Test3.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson18;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+//        arr[9] = 0;
+
+        int[][] arr2 = new int[3][];
+
+        System.out.println(arr2);
+
+    }
+}
