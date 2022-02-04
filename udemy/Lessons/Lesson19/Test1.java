624960d417c5aac792fb86580aad893d30a726ad
HERO <ivar110kg@gmail.com>
2022-02-04 05:31:02 +0400

    Add Test1.java

---
+++ udemy/Lessons/Lesson19/Test1.java
@@ -0,0 +1,8 @@
+package Lessons.Lesson19;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("args[0]: " + args[0]);
+        System.out.println("length: " + args.length);
+    }
+}
