f8af3009acb0c7065f2d1f06905cadf89949a93a
HERO <ivar110kg@gmail.com>
2022-02-04 17:52:05 +0400

    Add Test2.java

---
+++ udemy/Lessons/Lesson19/Test2.java
@@ -0,0 +1,26 @@
+package Lessons.Lesson19;
+
+public class Test2 {
+    static void sum(int... a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+
+    static void sum2(int[] a) {
+        int sum = 0;
+        for (int i = 0; i < a.length; i++) {
+            sum += a[i];
+        }
+        System.out.println(sum);
+    }
+
+    public static void main(String[] args) {
+        sum(1, 2, 3);
+        sum2(new int[]{1, 2, 3});
+    }
+
+
+}
