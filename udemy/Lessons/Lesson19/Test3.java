1fb72c9ddf276018c994e28aaaf88717bdea09c4
HERO <ivar110kg@gmail.com>
2022-02-05 05:41:30 +0400

    Add Test3.java

---
+++ udemy/Lessons/Lesson19/Test3.java
@@ -0,0 +1,17 @@
+package Lessons.Lesson19;
+
+public class Test3 {
+    public static void main(String[] args) {
+        int[] array = {0, 6, 4, 1};
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+}
