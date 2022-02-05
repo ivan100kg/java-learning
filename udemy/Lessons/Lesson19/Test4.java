ad0c29f999ade8d8adc96765a6337c7f691b0acb
HERO <ivar110kg@gmail.com>
2022-02-05 05:44:54 +0400

    Add Test4.java

---
+++ udemy/Lessons/Lesson19/Test4.java
@@ -0,0 +1,13 @@
+package Lessons.Lesson19;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int sum = 0;
+        int[] array = {0, 6, 4, 1};
+        for (int i : array) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+
+}
