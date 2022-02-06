c5de7bd16ed60f38b55d35bcb60785dd75f3b6f9
HERO <ivar110kg@gmail.com>
2022-02-06 19:02:53 +0400

    Correct homework19

--- udemy/Homeworks/hw19/Hw19.java
+++ udemy/Homeworks/hw19/Hw19.java
@@ -3,17 +3,26 @@ package Homeworks.hw19;
 public class Hw19 {
     public static String[] abc(String[]... arr) {
         int total = 0;
-        for (int i = 0; i < arr.length; i++) {
-            total += arr[i].length;
+        for (String[] s_arr : arr) {
+            total += s_arr.length;
         }
 
         String[] result = new String[total];
-        for (int i = 0, k = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                result[k] = arr[i][j];
-                k++;
+        int count = 0;
+        for (String[] arr1: arr){
+            for (String s : arr1){
+                result[count] = s;
+                count++;
             }
         }
+
+//        for (int i = 0, k = 0; i < arr.length; i++) {
+//            for (int j = 0; j < arr[i].length; j++) {
+//                result[k] = arr[i][j];
+//                k++;
+//            }
+//        }
+
         return result;
     }
 
