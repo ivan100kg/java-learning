239a117c689a2e545fcda271051fd3fafcf23094
HERO <ivar110kg@gmail.com>
2022-02-08 18:07:07 +0400

    Add Test6.java

---
+++ udemy/Lessons/Lesson21/Test6.java
@@ -0,0 +1,26 @@
+package Lessons.Lesson21;
+
+public class Test6 {
+    int abc() {
+        return 5;
+    }
+
+    int abc2(int i) {
+        if (i > 10) return 5;
+        else return 10;
+    }
+
+    void abc3(int i2) {
+        if (i2 > 3) return;
+        System.out.println("Hello");
+    }
+
+
+    public static void main(String[] args) {
+        int a = new Test6().abc();
+        new Test6().abc();
+        new Test6().abc2(12);
+
+
+    }
+}
