c21cd2da27592357ebc536d1e873bec1deb04efa
Ivan B <ivar110kg@gmail.com>
2022-02-15 17:46:27 +0400

    Do homework23

---
+++ udemy/Homeworks/hw23/Hw23.java
@@ -0,0 +1,11 @@
+package Homeworks.hw23;
+
+public class Hw23 {
+    /*
+        1 - compile time error
+        2 - compile time error
+        3 - "X"; "Y"; false
+        4 - compile error
+        5 - compile error
+    */
+}
