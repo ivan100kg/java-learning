d5d4a2f30067d28cff9b59db21d7d30d35b33040
Ivan B <ivar110kg@gmail.com>
2022-02-17 05:22:36 +0400

    Add Test5.java

--- udemy/Lessons/Lesson24/Test5.java
+++ udemy/Lessons/Lesson24/Test5.java
@@ -8,25 +8,36 @@ public class Test5 {
     }
 }
 
-interface I1{
+interface I1 {
+    static void foo() {
+        System.out.println("static");
+    }
+
     void abc();
-    default void def(){
+
+    default void def() {
         System.out.println("def method");
     }
 }
 
-interface I2 extends I1{
+interface I2 extends I1 {
     void abc();
 }
 
-abstract class R implements I1{
-    abstract public void abc();
+abstract class R implements I1 {
+    public static void main(String[] args) {
+        I1.foo();
+    }
 }
 
-class Zz implements I1{
+class Zz implements I1 {
 
     @Override
     public void abc() {
         System.out.println("abc method");
+    }
+
+    I1 Ret() {
+        return new Zz();
     }
 }
