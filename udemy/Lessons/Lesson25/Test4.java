72cfcf70032b70a67a49896cb994a6cfc027b0db
Ivan B <ivar110kg@gmail.com>
2022-02-19 14:35:56 +0400

    add the file Test4.java

---
+++ udemy/Lessons/Lesson25/Test4.java
@@ -0,0 +1,9 @@
+package Lessons.Lesson25;
+
+public class Test4 {
+    public static void main(String[] args) {
+        String[] arr1 = {"Hello", "Bye"};
+        Object[] arr2 = arr1;
+        String[] arr3 = (String[]) arr2;
+    }
+}
