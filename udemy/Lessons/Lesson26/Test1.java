6c90345b95ca8acbb64fa576410fbf5306de24ce
Ivan B <ivar110kg@gmail.com>
2022-02-21 19:35:38 +0400

    update Test1.java

--- udemy/Lessons/Lesson26/Test1.java
+++ udemy/Lessons/Lesson26/Test1.java
@@ -8,7 +8,9 @@ public class Test1 {
         Car c2 = new Car("red", "V4");
         Car c3 = new Car("black", "V8");
         Car c4 = new Car("black", "V8");
-        System.out.println(c1.equals(c2));
+        System.out.println(c1.toString());
+        System.out.println(c2.toString());
+        System.out.println(c3);
 
         // test
         /*Object o = new Car("blue", "1.6");
@@ -16,18 +18,19 @@ public class Test1 {
         System.out.println(((Car) o).color);
         System.out.println(c4.engine);*/
 
-        ArrayList <Car> list = new ArrayList<>();
+        ArrayList<Car> list = new ArrayList<>();
         list.add(c1);
         list.add(c2);
         list.add(c3);
-        System.out.println(list.contains(c4));
+        System.out.println(c1.equals(c2));
     }
 }
 
-class Car{
+class Car {
     String color;
     String engine;
-    Car(String color, String engine){
+
+    Car(String color, String engine) {
         this.color = color;
         this.engine = engine;
     }
@@ -37,9 +40,13 @@ class Car{
         if (obj instanceof Car) {
             Car c2 = (Car) obj;
             return color.equals(c2.color) && engine.equals(c2.engine);
-        }
-        else {
+        } else {
             return false;
         }
+    }
+
+    @Override
+    public String toString() {
+        return color + " car with " + engine;
     }
 }
