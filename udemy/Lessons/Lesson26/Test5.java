e99e2c911f25cfd07cb12c9f170b51d1bd119281
Ivan B <ivar110kg@gmail.com>
2022-02-21 19:36:38 +0400

    add Test5.java

---
+++ udemy/Lessons/Lesson26/Test5.java
@@ -0,0 +1,11 @@
+package Lessons.Lesson26;
+
+public class Test5 {
+    {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Test5 t = new Test5();
+    }
+}
