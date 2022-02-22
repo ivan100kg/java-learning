9b97444e672dd7a087d60450078908dd4537ac2a
Ivan B <ivar110kg@gmail.com>
2022-02-22 17:39:54 +0400

    add Test7.java

---
+++ udemy/Lessons/Lesson26/Test7.java
@@ -0,0 +1,49 @@
+package Lessons.Lesson26;
+
+public class Test7 {
+    public static void main(String[] args) {
+        Lion l = new Lion();
+    }
+}
+
+class Animal {
+    Animal() {
+        System.out.println("Constructor of Animal");
+    }
+
+    static {
+        System.out.println("static init in Animal");
+    }
+
+    {
+        System.out.println("non-static init in Animal");
+    }
+}
+
+class Mammal extends Animal {
+    Mammal() {
+        System.out.println("Constructor of Mammal");
+    }
+
+    static {
+        System.out.println("static init in Mammal");
+    }
+
+    {
+        System.out.println("non-static init in Mammal");
+    }
+}
+
+class Lion extends Mammal {
+    Lion() {
+        System.out.println("Constructor of Lion");
+    }
+
+    static {
+        System.out.println("static init in Lion");
+    }
+
+    {
+        System.out.println("non-static init in Lion");
+    }
+}
