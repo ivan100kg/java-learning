b729adeb8575dde8646e3618eceec9d4a03055fd
Ivan B <ivar110kg@gmail.com>
2022-02-22 17:39:19 +0400

    update Test5.java

--- udemy/Lessons/Lesson26/Test5.java
+++ udemy/Lessons/Lesson26/Test5.java
@@ -2,10 +2,35 @@ package Lessons.Lesson26;
 
 public class Test5 {
     {
-        System.out.println("Hello");
+        System.out.println("Initializer block 3");
+    }
+
+    Test5() {
+        System.out.println("Constructor 1");
+    }
+
+    Test5(int a) {
+        this();
+        System.out.println("Constructor 2");
+    }
+
+    {
+        System.out.println("Initializer block 1");
+    }
+
+    static {
+        System.out.println("Static block 1");
+    }
+
+    {
+        System.out.println("Initializer block 2");
+    }
+
+    static {
+        System.out.println("Static block 2");
     }
 
     public static void main(String[] args) {
-        Test5 t = new Test5();
+        Test5 t = new Test5(12);
     }
 }
