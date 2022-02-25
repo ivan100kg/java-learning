c9616b40afd79d09797fed56d0f38d481d539c87
Ivan B <ivar110kg@gmail.com>
2022-02-25 20:56:13 +0400

    update Test4.java

--- udemy/Lessons/Lesson27/Test4.java
+++ udemy/Lessons/Lesson27/Test4.java
@@ -1,17 +1,45 @@
 package Lessons.Lesson27;
 
+import java.io.*;
+
 public class Test4 {
-    public static void main(String[] args) {
-        int[] array = {4, 8, 1};
-        System.out.println("This is an array");
+    void abc() throws FileNotFoundException{
+        File f = new File("test10.txt");
+//        try {
+//            FileInputStream fis = new FileInputStream(f);
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Exception was caught " + e);
+//        }
+        FileInputStream fis = new FileInputStream(f);
+
+    }
+
+    void def() {
+        System.out.println("Hi");
         try {
-            System.out.println(array[1]);
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("ArrayIndexOutOfBoundsException was caught " + e);
-        } catch (Throwable e) {
-            System.out.println("Any throwable");
-        } finally {
-            System.out.println("Good day!");
+            abc();
         }
+        catch (FileNotFoundException e) {
+            System.out.println("Exception was caught " + e);
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+//        File f = new File("test10.txt");
+//
+//        try {
+//            FileInputStream fis = new FileInputStream(f);
+//            System.out.println("Hello!");
+//        } catch (FileNotFoundException e) {
+//            System.out.println("Exception was caught " + e);
+//        } catch (Throwable e) {
+//            System.out.println("Any throwable");
+//        } finally {
+//            System.out.println("Good day!");
+//        }
+//        System.out.println("After block");
+        Test4 t = new Test4();
+        t.def();
+        t.abc();
     }
 }
