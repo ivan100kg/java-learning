54370e8e2db99664d1319695a01063f79aad05b1
Ivan B <ivar110kg@gmail.com>
2022-02-28 19:19:08 +0400

    add Test17.java

---
+++ udemy/Lessons/Lesson27/Test17.java
@@ -0,0 +1,17 @@
+package Lessons.Lesson27;
+
+import java.util.ArrayList;
+
+public class Test17 {
+    static {
+        int a = Integer.parseInt("12");
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+        ArrayList<String> arr = new ArrayList<>();
+        while (true) {
+            arr.add(count + "");
+        }
+    }
+}
