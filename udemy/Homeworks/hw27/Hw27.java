9eae598788c1c0d86f002c5f9cea7624b13053b7
Ivan B <ivar110kg@gmail.com>
2022-03-01 20:44:48 +0400

    do homework27+28

---
+++ udemy/Homeworks/hw27/Hw27.java
@@ -0,0 +1,50 @@
+package Homeworks.hw27;
+
+public class Hw27 {
+    public static void main(String[] args) {
+        Tiger t = new Tiger();
+        t.eat("meat");
+        try {
+            t.drink("water");
+            try {
+                t.drink("beer");
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+            } finally {
+                System.out.println("Inner finally");
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class NoMeatException extends RuntimeException {
+    NoMeatException(String s) {
+        super(s);
+    }
+}
+
+class NoWaterException extends Exception {
+    NoWaterException(String s) {
+        super(s);
+    }
+}
+
+class Tiger {
+    void eat(String s) {
+        if (!s.equals("meat")) {
+            throw new NoMeatException("Tiger doesn't eat " + s);
+        } else {
+            System.out.println("Tiger eats a meat");
+        }
+    }
+
+    void drink(String s) throws NoWaterException {
+        if (!s.equals("water")) {
+            throw new NoWaterException("Tiger doesn't drink " + s);
+        } else {
+            System.out.println("Tiger drinks a water");
+        }
+    }
+}
