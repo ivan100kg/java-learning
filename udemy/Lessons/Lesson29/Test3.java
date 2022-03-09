7198a6df97ca577a6768ce0c4bab238c56aeb110
Ivan B <ivar110kg@gmail.com>
2022-03-09 20:03:58 +0400

    update Test6.java

--- udemy/Lessons/Lesson29/Test3.java
+++ udemy/Lessons/Lesson29/Test3.java
@@ -1,85 +1,85 @@
-package Lessons.Lesson29;
+//package Lessons.Lesson29;
+//
+//import java.util.ArrayList;
+//
+//public class Test3 {
+//    public static void main(String[] args) {
+//        ArrayList<Student> arr = new ArrayList<>();
+//        Student st1 = new Student("Ivan", 'm', 39, 5, 9.6);
+//        Student st2 = new Student("Egor", 'm', 31, 3, 8.4);
+//        Student st3 = new Student("Sarah", 'w', 18, 2, 7.7);
+//        Student st4 = new Student("Pedro", 'm', 23, 2, 2.5);
+//        Student st5 = new Student("Lisa", 'w', 64, 1, 5.0);
+//        arr.add(st1);
+//        arr.add(st2);
+//        arr.add(st3);
+//        arr.add(st4);
+//        arr.add(st5);
+//
+//        StudentInfo si = new StudentInfo();
+//
+//        System.out.println("fsog------------------------");
+//        si.testStudents(arr, (Student s) -> {
+//            return s.avgGrade > 8.5;
+//        });
+//        System.out.println("fsug------------------------");
+//        si.testStudents(arr, (Student s) -> {
+//            return s.avgGrade <= 8.5;
+//        });
+//        System.out.println("fsoa------------------------");
+//        si.testStudents(arr, (Student s) -> {
+//            return s.age > 25;
+//        });
+//        System.out.println("fsua------------------------");
+//        si.testStudents(arr, (Student s) -> {
+//            return s.age <= 25;
+//        });
+//        System.out.println("fsbs------------------------");
+//        si.testStudents(arr, (Student s) -> {
+//            return s.sex == 'm';
+//        });
+//        System.out.println("fsmc------------------------");
+//        si.testStudents(arr, (Student s) -> {
+//            return s.avgGrade <= 8.5 && s.sex == 'w' && s.course < 4;
+//        });
+//    }
+//}
 
-import java.util.ArrayList;
-
-public class Test3 {
-    public static void main(String[] args) {
-        ArrayList<Student> arr = new ArrayList<>();
-        Student st1 = new Student("Ivan", 'm', 39, 5, 9.6);
-        Student st2 = new Student("Egor", 'm', 31, 3, 8.4);
-        Student st3 = new Student("Sarah", 'w', 18, 2, 7.7);
-        Student st4 = new Student("Pedro", 'm', 23, 2, 2.5);
-        Student st5 = new Student("Lisa", 'w', 64, 1, 5.0);
-        arr.add(st1);
-        arr.add(st2);
-        arr.add(st3);
-        arr.add(st4);
-        arr.add(st5);
-
-        StudentInfo si = new StudentInfo();
-
-        System.out.println("fsog------------------------");
-        si.testStudents(arr, (Student s) -> {
-            return s.avgGrade > 8.5;
-        });
-        System.out.println("fsug------------------------");
-        si.testStudents(arr, (Student s) -> {
-            return s.avgGrade <= 8.5;
-        });
-        System.out.println("fsoa------------------------");
-        si.testStudents(arr, (Student s) -> {
-            return s.age > 25;
-        });
-        System.out.println("fsua------------------------");
-        si.testStudents(arr, (Student s) -> {
-            return s.age <= 25;
-        });
-        System.out.println("fsbs------------------------");
-        si.testStudents(arr, (Student s) -> {
-            return s.sex == 'm';
-        });
-        System.out.println("fsmc------------------------");
-        si.testStudents(arr, (Student s) -> {
-            return s.avgGrade <= 8.5 && s.sex == 'w' && s.course < 4;
-        });
-    }
-}
-
-class Student {
-    String name;
-    char sex;
-    int age;
-    int course;
-    double avgGrade;
-
-    Student(String name, char sex, int age, int course, double avgGrade) {
-        this.name = name;
-        this.sex = sex;
-        this.age = age;
-        this.course = course;
-        this.avgGrade = avgGrade;
-    }
-}
-
-class StudentInfo {
-    void printStudent(Student st) {
-        System.out.print("name: " + st.name);
-        System.out.print("\tsex: " + st.sex);
-        System.out.print("\tage: " + st.age);
-        System.out.print("\tcourse: " + st.course);
-        System.out.println("\tavgGrade: " + st.avgGrade);
-    }
-
-    void testStudents(ArrayList<Student> aL, StudentChecks sc) {
-        for (Student s : aL) {
-            if (sc.test(s)) {
-                printStudent(s);
-            }
-        }
-    }
-}
-
-interface StudentChecks {
-    boolean test(Student s);
-}
+//class Student {
+//    String name;
+//    char sex;
+//    int age;
+//    int course;
+//    double avgGrade;
+//
+//    Student(String name, char sex, int age, int course, double avgGrade) {
+//        this.name = name;
+//        this.sex = sex;
+//        this.age = age;
+//        this.course = course;
+//        this.avgGrade = avgGrade;
+//    }
+//}
+//
+//class StudentInfo {
+//    void printStudent(Student st) {
+//        System.out.print("name: " + st.name);
+//        System.out.print("\tsex: " + st.sex);
+//        System.out.print("\tage: " + st.age);
+//        System.out.print("\tcourse: " + st.course);
+//        System.out.println("\tavgGrade: " + st.avgGrade);
+//    }
+//
+//    void testStudents(ArrayList<Student> aL, StudentChecks sc) {
+//        for (Student s : aL) {
+//            if (sc.test(s)) {
+//                printStudent(s);
+//            }
+//        }
+//    }
+//}
+//
+//interface StudentChecks {
+//    boolean test(Student s);
+//}
 
